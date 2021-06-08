9d144568041a3530c27a11678ec5de40486b5359
manenshpatil06 <85335582+manenshpatil06@users.noreply.github.com>
2021-06-08 12:57:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function Compute(){
+
+    var principle = document.getElementById('principle').value;
+    var rate = document.getElementById('rate').value;
+    var years = document.getElementById('years').value;
+    var interest = (principle * years * rate) / 100;
+    document.getElementById("result").innerHTML="Simple interest is :" + interest ;
 }
-        
+
+function valid(){
+  var principle = document.getElementById('principle');
+  if (principle.value=="") {
+    alert("Please Enter principle Amount");
+    principle.focus();
+    return false;
+  }
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
